750753a7b5032111fc615fac07e959c03840763f
iiframdani <iiframdani@gmail.com>
2022-05-27 15:33:51 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#88C5A1',
+        secondary: '#64748B',
+        dark: '#171717',
+      },
+    },
+    screen: {
+      '2xl': '1320px',
+    }
+  },
+  plugins: [],
+}
